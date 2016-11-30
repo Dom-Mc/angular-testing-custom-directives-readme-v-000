e0d402aeccfff327c44b4b4dcd5449f77a96eba8
Dom McKellar <dom.mckellar@gmail.com>
2016-11-30 13:04:36 -0800

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -1,8 +1,8 @@
 describe('Directive Test', function() {
-	browser.get('http://localhost:8080');
+  var counter = element(by.css('.counter'));
+  var count = element(by.css('.counter__count'));
 
-	var counter = element(by.css('.counter'));
-	var count = element(by.css('.counter__count'));
+	browser.get('http://localhost:8080');
 
 	it('should have an initial 0 count', function () {
 		expect(count.getInnerHtml()).toEqual('Current count: 0');
@@ -10,7 +10,6 @@ describe('Directive Test', function() {
 
 	it('should increment when we click on it', function () {
 		counter.click();
-
 		expect(count.getInnerHtml()).toEqual('Current count: 1');
 	});
 
